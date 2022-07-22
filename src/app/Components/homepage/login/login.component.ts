676c7ec49ecd2a1b788d266360922ab3cb8ea0f0
Pratiksha Shende <pratiksha.shende@gep.com>
2022-07-22 10:33:38 +0530

    ----

--- src/app/Components/homepage/login/login.component.ts
+++ src/app/Components/homepage/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  submitted=false;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  form=new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(8)])
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    this.submitted = true;
+    console.log(this.form.value); //retrun all the form control values
+  
   }
 
 }
--- src/app/Components/homepage/signup/signup.component.ts
+++ src/app/Components/homepage/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
+submitted=false;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  form=new FormGroup({
+    username:new FormControl('',[Validators.required]),
+    contact:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(8)]),
+    confirmpassword: new FormControl('',[Validators.required])
+  });
+  get f() {
+    return this.form.controls;
   }
 
+  submit() {
+    this.submitted = true;
+    console.log(this.form.value); //retrun all the form control values
+  
+  }
 }
